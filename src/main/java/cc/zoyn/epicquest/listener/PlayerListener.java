faa597a22091f970c535ae32f0e008327bce9d57
Zoyn <602723113@qq.com>
2018-03-11 16:28:58 +0800

    :package::memo: update

--- src/main/java/cc/zoyn/epicquest/listener/PlayerListener.java
+++ src/main/java/cc/zoyn/epicquest/listener/PlayerListener.java
@@ -1,6 +1,8 @@
 package cc.zoyn.epicquest.listener;
 
+import cc.zoyn.epicquest.EpicQuest;
 import cc.zoyn.epicquest.dto.User;
+import cc.zoyn.epicquest.manager.QuestManager;
 import cc.zoyn.epicquest.manager.UserManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,8 +16,14 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+        // 以下为用户任务数据检查
         if (UserManager.getInstance().getUsers().stream().noneMatch(user -> user.getName().equals(event.getPlayer().getName()))) {
-            UserManager.getInstance().addUser(new User(event.getPlayer()));
+            // 若不存在数据, 则自动创建用户任务数据, 并且自动领取默认任务
+            User user = new User(event.getPlayer());
+            user.addQuest(QuestManager.getInstance().getQuestById(
+                    EpicQuest.getInstnace().getConfig().getInt("default-quest")).get());
+
+            UserManager.getInstance().addUser(user);
         }
     }
 
